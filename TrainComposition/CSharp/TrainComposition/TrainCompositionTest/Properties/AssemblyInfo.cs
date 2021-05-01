6b548c2f287110f88b8ff4b48208350eb8408c47
Tiago Ribeiro <tfnribeiro@gmail.com>
2021-05-01 19:27:48 +0200

    TrainComposition
    Fixed folder structure, double linked list applied to trains.

---
+++ TrainComposition/CSharp/TrainComposition/TrainCompositionTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TrainCompositionTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrainCompositionTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("865e6f8e-e0b7-4ced-9875-89b39c7316ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
